a2914982be0e54b4a68aea0ce00a987f7a407caa
charlescoudrais <charlescoudrais@gmail.com>
2011-11-24 22:40:06 +0100

    Une petite touche perso de la part d'un boulet. ;)

--- index.php
+++ index.php
@@ -12,4 +12,9 @@ function time2 ()
 }
 time1();
 time2();
-echo'Saaaallllllooooope!';
+echo'Saaaallllllooooope!<br />' . PHP_EOL;
+echo 'IL NE FAUT PAS PARLER COMME CA MEME SI TU AS UNE VIE DE MERDE, ET JE SAIS DE QUOI ON PARLE !<br />' . PHP_EOL;
+echo 'Avec tous le respect que je te dois. <br />' . PHP_EOL;
+echo 'Bon Courage, Charles.<br />' . PHP_EOL;
+
+echo 'je sais pas si je me rappellerai du pourquoi de ce message, j\'ai vraiment une vie qui pue du cul (la plupart du temps ;) ) '. PHP_EOL;
