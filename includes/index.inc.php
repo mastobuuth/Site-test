bccbc3eb9dcc8e6fff9e154612460833dd70ec65
charles coudrais <charlescoudrais@gmail.com>
2011-12-17 20:57:13 +0100

    17122011-2056
    Une vrai vie de merde 4 !

---
+++ includes/index.inc.php
@@ -0,0 +1,19 @@
+<?php
+$jnsmsleb = new JeNiqueSaMereSiLEstBonne();
+        $jnsmsleb->getTime(5);
+        $jnsmsleb->getAlert('Es-tu sûr qu\'elle soit si moche que ça ?');
+        $jnsmsleb->getTime(3);
+        $jnsmsleb->getAlert('je te crois sur parole...');
+        $jnsmsleb->getAlert('Et j\'en aurai fini, ceci a été codé très rapidement ;), on peut croire que je me la pete mais vraiment c\est à amélirer');
+        
+        /*
+        time1();
+        time2();
+        */
+        
+        echo'Saaaallllllooooope!<br />' . PHP_EOL;
+        echo 'IL NE FAUT PAS PARLER COMME CA MEME SI TU AS UNE VIE DE MERDE, ET JE SAIS DE QUOI ON PARLE !<br />' . PHP_EOL;
+        echo 'Avec tous le respect que je te dois. <br />' . PHP_EOL;
+        echo 'Bon Courage, Charles.<br />' . PHP_EOL;
+        
+        echo 'je sais pas si je me rappellerai du pourquoi de ce message, j\'ai vraiment une vie qui pue du cul (la plupart du temps ;) ) '. PHP_EOL;
---
+++ includes/page1.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h2>Page 1</h2>' . PHP_EOL;
+
+echo '<p>' . PHP_EOL;
+echo 'En reflichissant  un peu on devine ce qu\'il faut faire pour créer de nouvelle page...' . PHP_EOL;
+echo '</p>' . PHP_EOL;
